7f867599f2e67d5437e42c5cfc1ad2c9ecca936a
Joe Gasewicz <josefgasewicz@gmail.com>
2021-11-30 03:42:32 +0000

    Fixes no categories unload

--- StudyManager/ViewController.swift
+++ StudyManager/ViewController.swift
@@ -30,10 +30,12 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         // db at : /Users/josefgasewicz/Library/Containers/JRG-DEVELOPER-LTD.StudyManager/Data/Library/Application\ Support/StudyManager/
         CategorySelect.removeAllItems()
         getCategories()
-        selectedCategory = categories[0]
-        let name = selectedCategory?.name
-        tableName.stringValue = name!
-        getStudyItems(selectedCategoryName: selectedCategory?.name)
+        if categories.count > 0 {
+            selectedCategory = categories[0]
+            let name = selectedCategory?.name
+            tableName.stringValue = name!
+            getStudyItems(selectedCategoryName: selectedCategory?.name)
+        }
     }
     
     @IBAction func CategoryAddButton(_ sender: Any) {
